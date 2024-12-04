f583c34be55b4a3780b10bfccc6782a130578bd9
bilal <bilalachoudhry@gmail.com>
2024-12-03 20:58:46 -0500

    fixing type errors

--- frontend/src/api/smoothieApi.ts
+++ frontend/src/api/smoothieApi.ts
@@ -1,58 +1,62 @@
-import axios from 'axios';
-import { UpdateSmoothieInput } from './types';
+import axios from "axios";
+import { CreateSmoothieInput, UpdateSmoothieInput } from "./types";
+import { Smoothie } from "../Smoothies/context/SmoothiesProvider/types";
 
-const API_URL = "http://localhost:5001/api/smoothies"
+const API_URL = "http://localhost:5001/api/smoothies";
 
 const api = axios.create({
-    baseURL: API_URL,
-    headers: {
-        'Content-Type' : "application/json"
-    }
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-export const getSmoothies = async () => {
-    try {
-        const response = await api.get("");
-        return response.data  
-    } catch (error){
-        console.error("API error:", error)
-        throw new Error("Failed to fetch smoothies")
-    }
+export const getSmoothies = async (): Promise<Smoothie[]> => {
+  try {
+    const response = await api.get("");
+    return response.data;
+  } catch (error) {
+    console.error("API error:", error);
+    throw new Error("Failed to fetch smoothies");
+  }
 };
 
 export const getSmoothieById = async (id: string) => {
-    try {
-        const response = await api.get(`/${id}`);
-        return response.data;
-    } catch (error) {
-        throw new Error("Failed to fetch smoothie")
-    }
+  try {
+    const response = await api.get(`/${id}`);
+    return response.data;
+  } catch (error) {
+    throw new Error("Failed to fetch smoothie");
+  }
 };
 
-export const createSmoothie = async (smoothie: any) => {
-    try {
-        const response = await api.post("",smoothie)
-        return response.data;
-    } catch (error){
-        throw new Error("Failed to create new smoothie")
-    }
-}
+export const createSmoothie = async (
+  smoothie: CreateSmoothieInput
+): Promise<Smoothie> => {
+  try {
+    const response = await api.post("", smoothie);
+    return response.data;
+  } catch (error) {
+    throw new Error("Failed to create new smoothie");
+  }
+};
 
-export const updateSmoothie = async ({id, ...updates}: UpdateSmoothieInput) => {
-    try {
-        const response = await api.put(`/${id}`,updates);
-        return response.data
-    } catch (error){
-        throw new Error("Failed to update smoothie")
-    }
-}
+export const updateSmoothie = async ({
+  id,
+  ...updates
+}: UpdateSmoothieInput): Promise<Smoothie> => {
+  try {
+    const response = await api.put(`/${id}`, updates);
+    return response.data;
+  } catch (error) {
+    throw new Error("Failed to update smoothie");
+  }
+};
 
-export const deleteSmoothie = async (id: string) => {
-    try {
-        await api.delete(`${id}`);
-    } catch (error){
-        throw new Error("Failed to delete smoothie")
-    }
-}
-
-
+export const deleteSmoothie = async (id: string): Promise<void> => {
+  try {
+    await api.delete(`${id}`);
+  } catch (error) {
+    throw new Error("Failed to delete smoothie");
+  }
+};
--- frontend/src/api/types.ts
+++ frontend/src/api/types.ts
@@ -7,15 +7,42 @@ export interface Ingredient {
 export interface Tag {
     id: string;
     name: string;
+    createdAt: string;
+    updatedAt: string;
 }
 
 export interface Smoothie {
-    id: string;
-    name: string;
-    ingredients: Ingredient[]
-    tags: Tag[]
-    createdAt: string;
-    updatedAt: string;
+  id: string;
+  name: string;
+  ingredients: SmoothieIngredient[];
+  tags: SmoothieTag[];
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface SmoothieIngredient {
+  smoothieId: string;
+  ingredientId: string;
+  quantity: number;
+  unit: string;
+  createdAt: string;
+  updatedAt: string;
+  ingredient: IngredientDetails;
+}
+
+
+interface IngredientDetails {
+  id: string;
+  name: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface SmoothieTag {
+  smoothieId: string;
+  tagId: string;
+  createdAt: string;
+  tag: Tag;
 }
 
 export interface CreateSmoothieInput {
